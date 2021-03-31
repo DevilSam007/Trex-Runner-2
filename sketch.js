4ecc5f3be06daac2a2608de9a0e5b19c5926d794
DevilSam007 <73886649+DevilSam007@users.noreply.github.com>
2021-03-31 17:42:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -76,7 +76,7 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
   
-    if(keyDown("space") && trex.y >= 159) {
+    if(keyDown("space") && trex.y >= 359) {
       trex.velocityY = -12;
     }
   
